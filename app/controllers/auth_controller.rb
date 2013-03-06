a8e42d7edc26c158a59cad6fe01c94c00bf97883
Roman Brodetskiy <roman.brodetskiy@e-legion.com>
2013-03-06 17:31:31 +0400

    proper redirect uris

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,13 +5,13 @@ class AuthController < ApplicationController
     redirect_to oauth_client.auth_code.authorize_url(
       :scope => "http://picasaweb.google.com/data/",
       :access_type => "offline",
-      :redirect_uri => "http://localhost:3005/callback",
+      :redirect_uri => "http://dr-picasa.herokuapp.com/callback",
       :approval_prompt => 'force')
   end
 
   def callback
     access_token_obj = oauth_client.auth_code.get_token(params[:code],
-      :redirect_uri =>  "http://localhost:3005/callback",
+      :redirect_uri =>  "http://dr-picasa.herokuapp.com/callback",
       :token_method => :post)
     session[:token] = {
       :access_token => access_token_obj.token,
